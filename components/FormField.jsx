6ff92bb64ef5c4d525c999575e0c0fdd448a8fcc
sarvadandge29 <sarvadandge@gmail.com>
2025-02-27 14:41:59 +0530

    Added signIn logic and signup logic of user

--- components/FormField.jsx
+++ components/FormField.jsx
@@ -1,35 +1,35 @@
-import { Text, SafeAreaView, View, TextInput, TouchableOpacity, Image } from 'react-native'
-import React, { useState } from 'react'
+import { Text, SafeAreaView, View, TextInput, TouchableOpacity } from 'react-native';
+import React, { useState } from 'react';
 import { MaterialIcons } from '@expo/vector-icons';
 
 const FormField = ({ title, placeholder, keyboardType, value, handleChangeText, autoCapitalize }) => {
-    const [showPassword, setShowPassword] = useState(false)
+    const [showPassword, setShowPassword] = useState(false);
+
     return (
         <SafeAreaView className="mb-6">
             <Text className="text-sm font-medium text-gray-700 mb-2">{title}</Text>
-            <View className="flex-row">
+            <View className="flex-row items-center border border-gray-300 rounded-lg px-4">
                 <TextInput
-                    className="w-full h-12 px-4 border border-gray-300 rounded-lg focus:border-primary focus:outline-none"
+                    className="flex-1 h-12"
                     placeholder={placeholder}
                     keyboardType={keyboardType}
-                    secureTextEntry={(title == "Password" || title == "Confirm Password") && !showPassword}
+                    secureTextEntry={(title === "Password" || title === "Confirm Password") && !showPassword}
                     autoCapitalize={autoCapitalize}
                     onChangeText={handleChangeText}
                     value={value}
-
                 />
                 {(title === 'Password' || title === "Confirm Password") && (
                     <TouchableOpacity onPress={() => setShowPassword(!showPassword)}>
-                    <MaterialIcons
-                      name={showPassword ? 'visibility' : 'visibility-off'} // Toggle icon based on state
-                      size={24}
-                      color="black"
-                    />
-                  </TouchableOpacity>
+                        <MaterialIcons
+                            name={showPassword ? 'visibility' : 'visibility-off'}
+                            size={24}
+                            color="black"
+                        />
+                    </TouchableOpacity>
                 )}
             </View>
         </SafeAreaView>
-    )
-}
+    );
+};
 
-export default FormField
+export default FormField;
